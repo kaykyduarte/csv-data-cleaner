b86351a6e6caa780a15df53d0dfc8207a28cb6f4
Kayky Duarte Del Bosque <kaykyduarte222@gmail.com>
2026-07-08 17:18:43 -0300

    feat: implement remove nulls and duplicates to CSV data cleaner

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,4 +1,5 @@
 
+
 class CSVDataCleaner:
 
     def clean_name(self, dados):
@@ -14,3 +15,28 @@ class CSVDataCleaner:
             linha["cidade"] = linha["cidade"].strip().lower().title()
 
         return dados
+    
+    def remove_nulls(self, dados):
+        lista_limpa = []
+        colunas_obrigatorias = ["nome", "idade", "cidade"]
+        
+        for linha in dados:
+
+            if all(linha.get(coluna) is not None and str(linha.get(coluna)).strip() != "" for coluna in colunas_obrigatorias):
+                    lista_limpa.append(linha)
+            
+                 
+        return lista_limpa
+    
+    def remove_duplicates(self, dados):
+        lista_limpa = []
+        chaves_vistas = set()
+
+        for linha in dados:
+              assinatura = (linha["nome"], linha["idade"], linha["cidade"])
+
+              if assinatura not in chaves_vistas:
+                   lista_limpa.append(linha)
+                   chaves_vistas.add(assinatura)
+
+        return lista_limpa
